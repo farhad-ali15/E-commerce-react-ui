dddcef824dee44e8bac106b51bd8a3063dfd04fc
farhad-ali15 <farhadali15@yahoo.com>
2023-04-26 23:17:34 +0500

    half done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,100 @@
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
+import {
+  AddShoppingCartOutlined,
+  BadgeOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })}
 `;
+
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
+
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
+
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
-function Navbar() {
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
+
+const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          nobis magni enim? Quis praesentium quisquam recusandae quia libero
-          nesciunt? Soluta, ut reiciendis. Corporis incidunt obcaecati, magnam
-          sunt repellat omnis rerum.
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <SearchOutlined style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
         </Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Center>
+          <Logo>ABC INC.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <BadgeOutlined badgeContent={4} color="primary">
+              <AddShoppingCartOutlined />
+            </BadgeOutlined>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
-}
+};
 
 export default Navbar;
